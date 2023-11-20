16c8853d99ec873c48cb1f09b591b1fee75402d1
merlos <404446+merlos@users.noreply.github.com>
2023-11-20 16:26:07 +0300

    add support to stress in deb image. Update stress script.

--- scripts/run_cpu_test.sh
+++ scripts/run_cpu_test.sh
@@ -7,5 +7,5 @@ pods=$((`kubectl get pods -n $namespace |grep -i $clustername | wc -l` -1))
 
 for i in `seq 0 $pods`
 do
-  kubectl exec ${clustername}-drillbit-${i} --namespace=${namespace} -- /usr/bin/stress -c 3 &
+  kubectl exec ${clustername}bit-${i} --namespace=${namespace} -- /usr/bin/stress -c 3 &
 done
