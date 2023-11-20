9ef8bbbc9add019cf31136d48fc3afbc0137851b
merlos <404446+merlos@users.noreply.github.com>
2023-11-20 15:28:59 +0300

    set stop being verbose in drill

--- docker/drill/start.sh
+++ docker/drill/start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+# Be verbose 
 set -x
 DRILL_HOME="/opt/drill"
 DRILL_ENV_FILE="${DRILL_HOME}/conf/distrib-env.sh"
@@ -39,6 +41,9 @@ fi
 
 # Drillbit status
 ${DRILL_HOME}/bin/drillbit.sh status
+
+# Stop being verbose
+set +x
 
 # Keep container alive
 while true; do sleep 5; done
