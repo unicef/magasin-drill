6270c30cbd0e402785908fa817cbefb3d7520576
merlos <merlos@users.noreply.github.com>
2023-11-28 12:20:57 +0300

    Delete docker directory

--- docker/drill/isReady.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-DRILL_HOME="/opt/drill"
-
-${DRILL_HOME}/bin/drillbit.sh status
-exit $?
--- docker/zookeeper/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-/opt/zookeeper/bin/zkServer.sh start
-
-while true; do sleep 5; done
