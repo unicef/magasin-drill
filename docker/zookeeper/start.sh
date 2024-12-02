2c82f4414f9c46ec1df8d7e9feb09469df193f9d
merlos <merlos@gmail.com>
2024-12-02 15:32:28 +0300

    give more information when zkServer does not launch

--- docker/zookeeper/start.sh
+++ docker/zookeeper/start.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+figlet -f slant "Zookeeper"
 echo ZOOKEEPER_VERSION=$ZOOKEEPER_VERSION
+echo ZOO_LOG_DIR=$ZOO_LOG_DIR
+echo JAVA version= $(java -version)
+echo JAVA_HOME=$JAVA_HOME
 
 /opt/zookeeper/bin/zkServer.sh start
+
+# Keep container alive if previous command exited successfully
+if [[ $? -ne 0 ]]
+then
+  cat $ZOO_LOG_DIR/*
+
+  exit 1
+fi
 
 while true; do sleep 5; done
